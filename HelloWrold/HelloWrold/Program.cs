479a1da85ca94ffb97ed10385f5325c277fdc401
oul <oualoumd@gmail.com>
2017-01-29 22:28:23 -0500

    added hello  wrold program

---
+++ HelloWrold/HelloWrold/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWrold
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // this print out
+            Console.WriteLine("my first program");
+        }
+    }
+}
